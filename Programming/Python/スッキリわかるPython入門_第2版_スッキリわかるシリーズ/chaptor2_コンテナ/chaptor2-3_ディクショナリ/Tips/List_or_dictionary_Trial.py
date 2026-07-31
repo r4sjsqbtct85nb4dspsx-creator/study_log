a9b82a1b9ddbf116724ac3bd743cc75566810ffb
r4sjsqbtct85nb4dspsx-creator <r4sjsqbtct85nb4dspsx@gmail.com>
2026-07-31 21:49:55 +0900

    add:2-3

---
+++ Programming/Python/スッキリわかるPython入門_第2版_スッキリわかるシリーズ/chaptor2_コンテナ/chaptor2-3_ディクショナリ/Tips/List_or_dictionary_Trial.py
@@ -0,0 +1,7 @@
+#List:マラソン大会参加者を順位が高い順に登録する
+members = ["A", "B", "C", "D", "E"]
+print(f"優勝は{members[:1]}")
+
+#Dictionary:受験者のテストの点数を対応させて登録する
+scores = {"A":70, "B":60, "C":50}
+print(scores["A"])
